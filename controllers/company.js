62bf84bf04affce955eca296fd0b398590389ca7
Indra Setyajiwirawan Guritno <indrasetyajiwirawanguritno@gmail.com>
2019-05-03 12:01:41 +0700

    add controller

---
+++ controllers/company.js
@@ -0,0 +1,9 @@
+exports.test = (req, res) => { 
+    res.json("WELCOME TO COMPANY API!")
+}
+
+exports.login = (req, res) => { }
+
+exports.createEvent = (req, res) => { }
+
+exports.showEvent = (req, res) => { }
---
+++ controllers/vendor.js
@@ -0,0 +1,13 @@
+exports.test = (req, res) => { 
+    res.json("WELCOME TO VENDOR API!")
+}
+
+exports.login = (req, res) => { }
+
+exports.createEvent = (req, res) => { }
+
+exports.showEvent = (req, res) => { }
+
+exports.acceptEvent = (req, res) => { }
+
+exports.rejectEvent = (req, res) => { }
