f495e41b5d97e30b97c0684632e6089d6cb1ece9
Indra Setyajiwirawan Guritno <indrasetyajiwirawanguritno@gmail.com>
2019-05-03 12:00:54 +0700

    add app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+// Cors
+app.use(cors())
+
+// Body-Parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// MongoDB Connection
+const localDB = 'mongodb://127.0.0.1:27017/onlineBooking'
+const mongoDB = process.env.MONGO_STRINGCON || localDB
+mongoose.connect(mongoDB, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+})
+mongoose.Promise = global.Promise
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
+
+// Routes
+const index = require('./routes/index')
+app.use('/', index)
+
+// Port
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
+
+module.exports = app
