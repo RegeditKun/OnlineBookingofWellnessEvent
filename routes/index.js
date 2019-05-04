519c26b15a7c1a1df43b7229dbf22d36ae0feadb
Indra Setyajiwirawan Guritno <indrasetyajiwirawanguritno@gmail.com>
2019-05-04 10:38:01 +0700

    add new routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+const loginController = require('../controllers/login')
 const company = require('./API/company')
 const vendor = require('./API/vendor')
 
 router.use('/company', company)
 router.use('/vendor', vendor)
+router.use('/login', loginController.login)
 
 module.exports = router
