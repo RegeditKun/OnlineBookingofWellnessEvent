cbea59232a280e1feaf1f3682a31c01f043733c6
Indra Setyajiwirawan Guritno <indrasetyajiwirawanguritno@gmail.com>
2019-05-07 05:55:17 +0700

    update apidoc

--- doc/apidoc/api_project.js
+++ doc/apidoc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-05-06T13:09:17.898Z",
+    "time": "2019-05-06T22:52:59.484Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
