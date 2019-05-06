4f719b2c9d4f7216536ab35d2ce99f21d08a5d08
Indra Setyajiwirawan Guritno <indrasetyajiwirawanguritno@gmail.com>
2019-05-06 20:23:05 +0700

    move apidoc to apidoc.txt

--- routes/API/company.js
+++ routes/API/company.js
@@ -3,40 +3,12 @@ const router = express.Router()
 const companyControllers = require('../../controllers/company')
 const auth = require('../../middleware/auth')
 
-/**
- * @apiName Company Test API
- * @apiGroup Company
- * @api {get} /v1/api/company/test Get Test API
- * @apiHeader {String} auth Unique token
- *
- * @apiSuccess {json} WELCOME TO COMPANY API.
- */
-
 router.post('/registration', companyControllers.registration)
 
 router.get('/event', auth.authCompany, companyControllers.showEvent)
 
-router.post('/booking', auth.authCompany, companyControllers.createBooking)
-
-router.get('/booking', auth.authCompany, companyControllers.showBooking)
-
-/**
- * @apiName Get Event
- * @apiGroup Vendor
- * @api {get} /v1/api/company/event Get Event
- * @apiHeader {String} auth Unique token.)
- * @apiSuccess {Boolean} success Success status.
- * @apiSuccess {String} message Show all event.
- * @apiSuccess {Object[]} data Show all Event.
- */
-/**
- * @apiName Create Booking
- * @apiGroup Vendor
- * @api {get} /v1/api/company/booking/:id Get Event
- * @apiHeader {String} auth Unique token.)
- * @apiSuccess (Success 201) {Boolean} success Success status.
- * @apiSuccess (Success 201) {String} message Show all event.
- * @apiSuccess (Success 201) {Object[]} data Show all Event.
- */
+router.route('/booking')
+  .post(auth.authCompany, companyControllers.createBooking)
+  .get(auth.authCompany, companyControllers.showBooking)
 
 module.exports = router
